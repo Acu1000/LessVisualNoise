d23cff3740862f0f7ff5f22ddf18062dcb203c21
Acu1000 <140196544+Acu1000@users.noreply.github.com>
2023-08-06 16:44:51 +0200

    Update ModClass.cs

--- ModClass.cs
+++ ModClass.cs
@@ -2,7 +2,9 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Numerics;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UObject = UnityEngine.Object;
 
 namespace LessVisualNoise
@@ -19,6 +21,9 @@ namespace LessVisualNoise
         //    };
         //}
 
+        public override string GetVersion() => "v1.0.7.0";
+
+
         public LessVisualNoise() : base("LessVisualNoise")
         {
             Instance = this;
@@ -26,11 +31,40 @@ namespace LessVisualNoise
 
         public override void Initialize(Dictionary<string, Dictionary<string, GameObject>> preloadedObjects)
         {
-            Log("Initializing");
-
-            Instance = this;
-
-            Log("Initialized");
+            On.GameManager.OnNextLevelReady += OnSceneLoad;
         }
+
+        public void OnSceneLoad(On.GameManager.orig_OnNextLevelReady orig, global::GameManager self)
+        {
+            orig(self);
+            CreateBlackBackground();
+            RemoveBackgroundObjects();
+        }
+
+        private void CreateBlackBackground()
+        {
+            var blurPlane = GameObject.Find("BlurPlane");
+            if (blurPlane != null)
+            {
+                blurPlane.SetActive(false);
+            }
+
+            var black = GameObject.CreatePrimitive(PrimitiveType.Plane);
+            black.GetComponent<MeshRenderer>().material = new Material(Shader.Find("Sprites/Default"))
+            {
+                color = new Color(0, 0, 0, 255)
+            };
+            black.transform.position = new UnityEngine.Vector3(0f, 0f, 2f);
+            black.transform.localScale = new UnityEngine.Vector3(1000f, 1f, 1000f);
+            black.transform.rotation = UnityEngine.Quaternion.Euler(90f, 180f, 0f);
+        }
+
+        private void RemoveBackgroundObjects() // TODO
+        {
+
+            var scene = UnityEngine.SceneManagement.SceneManager.GetActiveScene();
+            
+        }
+
     }
 }
